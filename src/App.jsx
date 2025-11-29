57030a290d03c115adfefcd26d72a834c44f838a
Vinod Mali <snvinod09@gmail.com>
2025-11-29 15:13:11 +0530

    bug-fix: added fallback for meta.env

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,26 @@
 import { BrowserRouter, Routes, Route, Navigate, useLocation } from "react-router-dom";
 import { useEffect } from "react";
 import ReactGA from "react-ga4";
-
 import Dashboard from "./pages/Dashboard";
 import Projects from "./pages/Projects";
 import ProjectDetail from "./pages/ProjectDetail";
 
 const TRACKING_ID = import.meta.env.VITE_GA_TRACKING_ID;
 
-ReactGA.initialize(TRACKING_ID);
+if (TRACKING_ID) {
+  ReactGA.initialize(TRACKING_ID);
+}
 
 function RouteChangeTracker() {
   const location = useLocation();
 
   useEffect(() => {
-    ReactGA.send({
-      hitType: "pageview",
-      page: location.pathname + location.search,
-    });
+    if (TRACKING_ID) {
+      ReactGA.send({
+        hitType: "pageview",
+        page: location.pathname + location.search,
+      });
+    }
   }, [location]);
 
   return null;
