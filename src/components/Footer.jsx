18d6ea5d71362ed92b1d83c481464aad59f6209b
Vinod Mali <snvinod09@gmail.com>
2026-01-30 22:03:49 +0530

    fixed footer padding

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -15,9 +15,9 @@ const Footer = () => {
 
   return (
     // Footer container
-    <footer className="pb-20 max-w-5xl mx-auto px-6 text-center text-gray-600 dark:text-gray-400">
+    <footer className="pb-20 max-w-5xl mx-auto md:px-6 text-center text-gray-600 dark:text-gray-400">
       {/* Top Section: Contact, Navigation, and Social Links */}
-      <div className="max-w-5xl mx-auto px-4 md:px-0 flex flex-col md:flex-row justify-between items-start md:items-center py-8 border-t border-gray-200 dark:border-neutral-800 border-b border-gray-200 dark:border-neutral-800">
+      <div className="max-w-5xl mx-auto flex px-4 md:px-0 flex-col md:flex-row justify-between items-start md:items-center py-8 border-t border-gray-200 dark:border-neutral-800 border-b border-gray-200 dark:border-neutral-800">
         {/* Contact Information */}
         <div className="mb-8 md:mb-0 text-start">
           <p className="text-lg text-gray-700 dark:text-gray-300 font-light mb-1">
